d070941efcfb5023c1dc6a09bf356ac140dac658
Daniel Teske <daniel.teske@meteogroup.com>
2017-02-05 00:37:11 +0100

    selecting a random commemorative day in GetTodaysCommemorativeDay and GetUpcomingCommemorativeDay

--- index.js
+++ index.js
@@ -5,63 +5,50 @@
  */
 
 var Alexa = require('alexa-sdk');
-var commemorationDays = require('./commemoration-days-repository.json');
+var COMMEMORATION_DAYS = require('./commemoration-days-repository.json');
 
 var APP_ID = undefined; //OPTIONAL: replace with "amzn1.echo-sdk-ams.app.[your-unique-value-here]";
 var SKILL_NAME = 'Space Facts';
 
-/**
- * Array containing space facts.
- */
-var FACTS = [
-    "A year on Mercury is just 88 days long.",
-    "Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.",
-    "Venus rotates counter-clockwise, possibly because of a collision in the past with an asteroid.",
-    "On Mars, the Sun appears about half the size as it does on Earth.",
-    "Earth is the only planet not named after a god.",
-    "Jupiter has the shortest day of all the planets.",
-    "The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.",
-    "The Sun contains 99.86% of the mass in the Solar System.",
-    "The Sun is an almost perfect sphere.",
-    "A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.",
-    "Saturn radiates two and a half times more energy into space than it receives from the sun.",
-    "The temperature inside the Sun can reach 15 million degrees Celsius.",
-    "The Moon is moving approximately 3.8 cm away from our planet every year."
-];
-
-exports.handler = function(event, context, callback) {
+exports.handler = function (event, context, callback) {
+  try {
     var alexa = Alexa.handler(event, context);
     alexa.APP_ID = APP_ID;
     alexa.registerHandlers(handlers);
     alexa.execute();
+  } catch (e) {
+    console.error("cannot execute! ", e);
+  }
 };
 
 var handlers = {
-    'LaunchRequest': function () {
-        this.emit('GetFact');
-    },
-    'GetNewFactIntent': function () {
-        this.emit('GetFact');
-    },
-    'GetFact': function () {
-        // Get a random space fact from the space facts list
-        var factIndex = Math.floor(Math.random() * FACTS.length);
-        var randomFact = FACTS[factIndex];
+  'LaunchRequest': function () {
+    this.emit('GetTodaysCommemorativeDay');
+  },
+  'GetTodaysCommemorativeDay': function () {
+    var cd = getRandomCd();
+    var speechOutput = "Today's commemorative day is " + cd.name;
+    this.emit(':tellWithCard', speechOutput, SKILL_NAME, cd.day);
+  },
+  'GetUpcomingCommemorativeDay': function () {
+    var cd = getRandomCd();
+    var formattedDate = formatCdDate(cd);
+    var speechOutput = "Upcoming commemorative day is the " + cd.name + " on " + formattedDate;
+    this.emit(':tellWithCard', speechOutput, SKILL_NAME, cd.day);
+  }
+};
 
-        // Create speech output
-        var speechOutput = "Here's your fact: " + randomFact;
+function formatCdDate(cd) {
+  return new Date(new Date().getFullYear(), cd.month - 1, cd.day - 1, 0, 0, 0, 0);
+}
 
-        this.emit(':tellWithCard', speechOutput, SKILL_NAME, randomFact)
-    },
-    'AMAZON.HelpIntent': function () {
-        var speechOutput = "You can say tell me a space fact, or, you can say exit... What can I help you with?";
-        var reprompt = "What can I help you with?";
-        this.emit(':ask', speechOutput, reprompt);
-    },
-    'AMAZON.CancelIntent': function () {
-        this.emit(':tell', 'Goodbye!');
-    },
-    'AMAZON.StopIntent': function () {
-        this.emit(':tell', 'Goodbye!');
-    }
+function getRandomCd(){
+  var randomDayIndex = Math.floor(Math.random() * COMMEMORATION_DAYS.length);
+  return COMMEMORATION_DAYS[randomDayIndex];
+}
+
+this.filterCurrentCds = function(referenceDate){
+  return COMMEMORATION_DAYS.filter(function(cd){
+    return cd.month == referenceDate.getMonth()+1 && cd.day == referenceDate.getDate();
+  })
 };
