7eacfca930f9a360eb87db86cd5bbb9ae64eddf4
Daniel Teske <daniel.teske@meteogroup.com>
2017-02-05 00:38:24 +0100

    first test to check the lookup mechanisms

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -1,7 +1,19 @@
 let index = require("../index");
 
-describe("Dummy", () => {
+describe("Lookup", () => {
+
+  const MARCH_14 = new Date(2017, 2, 14, 0, 0, 0, 0);
+
+  it("should find current commemoration days", () => {
+    let cdsOnMarch14 = index.filterCurrentCds(MARCH_14);
+    expect(cdsOnMarch14.length).toBe(2);
+  });
+
+});
+
+describe("Formatting", () => {
   it("should be fine", () => {
     expect(true).toBeTruthy();
   });
 });
+
