ed05da83c2abe767bc6ab65405372f8531a1ae33
Daniel Teske <daniel.teske@meteogroup.com>
2017-02-05 00:37:42 +0100

    build now only bundles the production code

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
+rm -r node_modules
+rm commy-skill.zip
+npm install --production
+zip -r commy-skill.zip index.js commemoration-days-repository.json node_modules
+
+# reload dev dependencies
 npm install
-zip -r commy-skill.zip index.js node_modules
